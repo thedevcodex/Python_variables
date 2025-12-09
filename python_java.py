8d46961310443dcfff908f4df034ffce0ef2550d
TheDevJournal <s.sudha19082006@gmail.com>
2025-12-09 22:39:48 +0530

    Create python_java.py

---
+++ python_java.py
@@ -0,0 +1,15 @@
+'''You have:
+
+python = 10
+java = python
+python = 20
+
+
+Print java. Explain why the value is that.
+'''
+python=10 # I assign python =10 
+java=python#java=python means python have 10 so java=python means java=10(python value)
+python=20#now i am change python value to 20
+print(java)
+print(python)
+#Note i change Python as 20 at last but it didnt affect java because at first i assign python =10then i said java=python so first line assign the value 10 to python second value python assigned to java so third i change python value it didnt get change
