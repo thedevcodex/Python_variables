fc50c52c14537c46f5ad544b6fe4f010e1078a7e
TheDevJournal <s.sudha19082006@gmail.com>
2025-12-09 22:36:15 +0530

    Create name_age.py

---
+++ name_age.py
@@ -0,0 +1,6 @@
+#Take a user input for name and age and print:
+#"Hello <name>, you will turn 100 after <years> years"
+name="sam"
+age=19
+years=2025+81
+print(f"Hello {name}, you will turn 100 after {years} years")
