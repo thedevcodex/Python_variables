ae28f16b3c4cf33f7dcc20192b0a378b0c6c44f1
TheDevJournal <s.sudha19082006@gmail.com>
2025-12-09 22:38:15 +0530

    Create increase_multiply_subtract.py

---
+++ increase_multiply_subtract.py
@@ -0,0 +1,16 @@
+'''You have x = 10. Do:
+
+increase x by 5
+
+multiply x by 3
+
+subtract 7
+Print x after each step.'''
+x=10
+increase=a+5
+multiply=x*3
+subract=x-7
+print(x)
+print(increase)
+print(subract)
+print(multiply)
