8efc4660c084cabf07d5848549fa154d7bc5ea34
TheDevJournal <s.sudha19082006@gmail.com>
2025-12-09 22:38:51 +0530

    Create simple_interest.py

---
+++ simple_interest.py
@@ -0,0 +1,7 @@
+'''Write a program using variables to calculate simple interest:
+SI = (P * T * R) / 100'''
+P=5000 #principal amount
+T=2  #Time in 2 Years
+R=10 #interest rate
+SI=(P*T*R)/100
+print(SI)
