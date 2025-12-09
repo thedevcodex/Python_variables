7e81e99455c07a7ac9498133d6dc4787e804ed33
TheDevJournal <s.sudha19082006@gmail.com>
2025-12-09 22:35:20 +0530

    Create marks_percentage.py

---
+++ marks_percentage.py
@@ -0,0 +1,8 @@
+#Store your marks in tamil, english, maths. Print total and percentage.
+tamil=98
+english=90
+maths=70
+total=tamil+english+maths
+percentage=total/300*100
+print(f"Total: {total} ")
+print(f"Percentage : {percentage}%")
