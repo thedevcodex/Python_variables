490a7ff49098766729524687621abdd5de71dda7
TheDevJournal <s.sudha19082006@gmail.com>
2025-12-09 22:40:33 +0530

    Create conversion_calculation.py

---
+++ conversion_calculation.py
@@ -0,0 +1,5 @@
+#You get a user input age as string. Convert to integer and check how many years left for age 60.
+age="19"
+convert=int(age)
+cal=((60-convert)+2025)-2025
+print(f"Now I am {age} i reach 60 it take {cal} years")
