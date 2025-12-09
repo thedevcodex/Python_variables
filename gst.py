84bc0a5c4f9458262ef1535a68b537cdb9485f2f
TheDevJournal <s.sudha19082006@gmail.com>
2025-12-09 22:36:39 +0530

    Create gst.py

---
+++ gst.py
@@ -0,0 +1,7 @@
+'''A shopkeeper increases price by 18% GST.
+Store original price in variable. Calculate final price using GST variable.
+'''
+original_price=250
+gst=18
+final_price=original_price+(original_price*gst/100)
+print(final_price)
