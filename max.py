83fa879fef8f2ab674d503bc3e89cb3cf731702d
TheDevJournal <s.sudha19082006@gmail.com>
2025-12-09 22:37:11 +0530

    Create max.py

---
+++ max.py
@@ -0,0 +1,16 @@
+#Store 5 numbers in variables and find the maximum number without using max().
+a=60
+b=20
+c=30
+d=40
+e=20
+if a>b and a>c and a>d and a>e:
+    print(a)
+elif b>a and b>c and b>d and b>e:
+    print(b)
+elif c>a and c>b and c>d and c>e:
+    print(c)
+elif d>a and d>b and d>c and d>e:
+    print(d)
+else:
+    print(e)
