403d7d8e0681e356894caf50332ab6556d443d0d
TheDevJournal <s.sudha19082006@gmail.com>
2026-03-02 03:50:23 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,40 @@
+#Create a variable age and assign your age. Print its type.
+age=19
+print(type(age))
+print("--------------------------------------------------------------------------------------------------")
+#Store your height in meters (example: 1.75). Print the datatype.
+height=156.8
+print(type(height))
+print("--------------------------------------------------------------------------------------------------")
+#Create a string variable city and print it in uppercase.
+city="coimbatore"
+print(city.upper())
+print("--------------------------------------------------------------------------------------------------")
+#Create a boolean variable is_student and print its type.
+is_student=True
+print(type(is_student))
+print("--------------------------------------------------------------------------------------------------")
+#Add two integer variables and print the result.
+a=19
+b=15
+print(a+b)
+print("--------------------------------------------------------------------------------------------------")
+#Multiply a float and an integer and print the result.
+a=10
+b=8.23
+print(a+b)
+print("--------------------------------------------------------------------------------------------------")
+#Convert integer 10 into float.
+a=float(10)
+print(a)
+print("--------------------------------------------------------------------------------------------------")
+#Convert float 9.8 into integer.
+x=int(9.8)
+print(x)
+print("--------------------------------------------------------------------------------------------------")
+x=int("100")
+print(x+50)
+print("--------------------------------------------------------------------------------------------------")
+#Print the type of "True" and True (observe difference).
+print(type("True"))
+print(type(True))
