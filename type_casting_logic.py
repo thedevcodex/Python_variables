267b63ca01decd6f55a910f946600603f449387d
TheDevJournal <s.sudha19082006@gmail.com>
2026-03-02 04:15:11 +0530

    Create type_casting_logic.py

---
+++ type_casting_logic.py
@@ -0,0 +1,59 @@
+'''Take two numbers as strings:
+
+a = "25"
+b = "5"
+
+Convert them and perform:
+
+Addition
+
+Division'''
+a="25"
+b="5"
+print(int(a)+int(b))
+print("-----------------------------------------------------------------------------------------")
+'''Create a variable price = 199.99.
+Convert it into:
+
+Integer
+
+String'''
+price=199.99
+print(int(price))
+print(str(price))
+print(type(str(price)))
+print("-----------------------------------------------------------------------------------------")
+#Check if a number is even using boolean expression.
+num=4
+result=num%2==0
+print(result)
+print("-----------------------------------------------------------------------------------------")
+'''Create a variable num = 15.
+Print:
+
+Is greater than 10?
+
+Is less than 5?
+'''
+num=15
+greater=num>10
+less=num<5
+print("Num is Greater than 10 ?",greater)
+print("Num is less than 5?",less)
+print("-----------------------------------------------------------------------------------------")
+'''What happens if you do:
+
+result = "10" + "20"
+
+Print and explain.''' # it wi;; print 1020 beacuse it is strings so it will concate
+result="10"+"20"
+print(result)
+print("-----------------------------------------------------------------------------------------")
+'''What happens if you do:
+
+result = 10 + 20.5
+
+Check the datatype of result'''
+result=10+20.5
+print(result)
+print("-----------------------------------------------------------------------------------------")
